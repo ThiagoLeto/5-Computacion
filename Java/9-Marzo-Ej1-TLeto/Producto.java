28ab232f768a141ad6e85ea66c353238546afcdd
ThiagoLeto <81710270+ThiagoLeto@users.noreply.github.com>
2022-03-16 10:03:21 -0300

    Add files via upload

---
+++ Java/9-Marzo-Ej1-TLeto/Producto.java
@@ -0,0 +1,15 @@
+public class Producto {
+    private String nombre;
+    private float precio;
+
+    public void Registro (String nombre, float precio) {
+
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+
+    public float Importe_pagar () {
+        return this.precio;
+    }
+}
