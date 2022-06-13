b4f8114fbe594b1569fb1ddd55394ecb4aeeee99
ThiagoLeto <81710270+ThiagoLeto@users.noreply.github.com>
2022-06-13 00:01:31 -0300

    Update main.js

--- Taller/Tateti/Tateti-diferente/main.js
+++ Taller/Tateti/Tateti-diferente/main.js
@@ -5,33 +5,93 @@ const cuadrado_usado1 = [];
 const cuadrado_usado2 = [];
 
 function cambiar(boton){
-    cont++;
-    cuadrado = document.getElementById(boton); //tiene el objeto de html
-    cuadrado1=boton;  //tiene el numero de id
-    let usado = false;
-
     
-    for (let i = 0; i < cuadrado_usado1[cuadrado_usado1.length-1]; i++) {
+    let cuadrado = document.getElementById(boton); 
+    let cuadrado1=boton;  
+    let cont1 = 0;
+    cont1 = cont%2;
+    
+    if(cont1==0){
+            if(cuadrado_usado1.includes(cuadrado1) || cuadrado_usado2.includes(cuadrado1)){
+                console.log("Ya esta agregado manin");
+            }
+            else{
+                cuadrado.style.backgroundColor = "orange";  
+                cuadrado_usado1.push(cuadrado1); 
+                console.log("Se agrega naranja:  " + cuadrado_usado1);
+                console.log(" ");
+                cont++;
+            }
         
-        if(cuadrado_usado1[i] == cuadrado1){
-            usado=true;
-            //console.log('llegue manin');  
+    }
+
+    else if(cont1==1){
+        if(cuadrado_usado2.includes(cuadrado1) || cuadrado_usado1.includes(cuadrado1)){
+            console.log("Ya esta agregado manin"); 
         }
-    }
-   
-    let cont1 = cont%2;
-    if(cont1==0 && usado == false){
-        cuadrado.style.backgroundColor = "orange";  
-        //console.log('par');
-        cuadrado_usado1.push(boton);
-    }
-    else if(cont1==1 && usado == false){
-        cuadrado.style.backgroundColor = "#31363a";      
-        //console.log('impar');
-        cuadrado_usado2.push(boton);
-    }
-
-    console.log("Naranjas manin  "+cuadrado_usado1);
-    console.log("Negros manito  "+cuadrado_usado2);
-
+        else{
+            cuadrado.style.backgroundColor = "#31363a";
+            cuadrado_usado2.push(cuadrado1); 
+            console.log("SE AGREGA NEGRO: " + cuadrado_usado2);
+            console.log(" ");
+            cont++;
+        }
+    }  
+    ganador();
 }
+
+
+function ganador(){
+    if (cuadrado_usado1.includes(1) && cuadrado_usado1.includes(2) && cuadrado_usado1.includes(3) || cuadrado_usado2.includes(1) && cuadrado_usado2.includes(2) && cuadrado_usado2.includes(3)) {
+        alert("El jugador 1 es el ganador manin!");}
+    else if (cuadrado_usado1.includes(1) && cuadrado_usado1.includes(4) && cuadrado_usado1.includes(7) || cuadrado_usado2.includes(1) && cuadrado_usado2.includes(2) && cuadrado_usado2.includes(3)) {
+        alert("El jugador 1 es el ganador manin!");}
+    else if (cuadrado_usado1.includes(2) && cuadrado_usado1.includes(5) && cuadrado_usado1.includes(8) || cuadrado_usado2.includes(1) && cuadrado_usado2.includes(2) && cuadrado_usado2.includes(3)) {
+        alert("El jugador 1 es el ganador manin!");}
+    else if (cuadrado_usado1.includes(3) && cuadrado_usado1.includes(6) && cuadrado_usado1.includes(9) || cuadrado_usado2.includes(1) && cuadrado_usado2.includes(2) && cuadrado_usado2.includes(3)) {
+        alert("El jugador 1 es el ganador manin!");}
+    else if (cuadrado_usado1.includes(4) && cuadrado_usado1.includes(5) && cuadrado_usado1.includes(6) || cuadrado_usado2.includes(1) && cuadrado_usado2.includes(2) && cuadrado_usado2.includes(3)) {
+        alert("El jugador 1 es el ganador manin!");}
+    else if (cuadrado_usado1.includes(7) && cuadrado_usado1.includes(8) && cuadrado_usado1.includes(9) || cuadrado_usado2.includes(1) && cuadrado_usado2.includes(2) && cuadrado_usado2.includes(3)) {
+        alert("El jugador 1 es el ganador manin!");}
+    else if (cuadrado_usado1.includes(1) && cuadrado_usado1.includes(5) && cuadrado_usado1.includes(9) || cuadrado_usado2.includes(1) && cuadrado_usado2.includes(2) && cuadrado_usado2.includes(3)) {
+        alert("El jugador 1 es el ganador manin!");}
+    else if (cuadrado_usado1.includes(3) && cuadrado_usado1.includes(5) && cuadrado_usado1.includes(7) || cuadrado_usado2.includes(1) && cuadrado_usado2.includes(2) && cuadrado_usado2.includes(3)) {
+        alert("El jugador 1 es el ganador manin!");}
+    
+    if(cuadrado_usado2.includes(1) && cuadrado_usado2.includes(2) && cuadrado_usado2.includes(3)){
+        alert("El jugador 2 es el ganador manin!");}
+    else if(cuadrado_usado2.includes(1) && cuadrado_usado2.includes(4) && cuadrado_usado2.includes(7)){
+        alert("El jugador 2 es el ganador manin!");}
+    else if(cuadrado_usado2.includes(3) && cuadrado_usado2.includes(6) && cuadrado_usado2.includes(9)){
+        alert("El jugador 2 es el ganador manin!");}
+    else if(cuadrado_usado2.includes(4) && cuadrado_usado2.includes(5) && cuadrado_usado2.includes(6)){
+        alert("El jugador 2 es el ganador manin!");}
+    else if(cuadrado_usado2.includes(7) && cuadrado_usado2.includes(8) && cuadrado_usado2.includes(9)){
+        alert("El jugador 2 es el ganador manin!");}
+    else if(cuadrado_usado2.includes(2) && cuadrado_usado2.includes(5) && cuadrado_usado2.includes(8)){
+        alert("El jugador 2 es el ganador manin!");}
+    else if(cuadrado_usado2.includes(1) && cuadrado_usado2.includes(5) && cuadrado_usado2.includes(9)){
+        alert("El jugador 2 es el ganador manin!");}
+    else if(cuadrado_usado2.includes(3) && cuadrado_usado2.includes(5) && cuadrado_usado2.includes(7)){
+        alert("El jugador 2 es el ganador manin!");}
+}
+
+
+function reiniciar(boton){
+    let botonardo = document.getElementById(boton); 
+    let botonardo1 = boton; 
+
+    for (let i = 0; i < cuadrado_usado1.length; i++) {
+        var id_cuadrado = document.getElementById(1);
+        id_cuadrado.style.backgroundColor = null;  
+    }
+    for (let i = 0; i < cuadrado_usado2.length; i++) {
+        var id_cuadrado = document.getElementById(i+1);
+        id_cuadrado.style.backgroundColor = null;  
+    }
+}
+
+
+//function jugar_de_nuevo(boton){}
+
